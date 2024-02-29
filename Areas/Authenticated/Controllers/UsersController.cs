3f754ba306c26bf6f7f8135f2cbc2ada53701d8b
NuclearSherwin <phongspacenasa@gmail.com>
2024-03-01 00:39:34 +0900

    push login/logic

---
+++ Areas/Authenticated/Controllers/UsersController.cs
@@ -0,0 +1,115 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using WebEnterprise.Data;
+using WebEnterprise.Models;
+
+namespace WebEnterprise.Areas.Authenticated.Controllers;
+
+[Area(Constants.Areas.AuthenticatedArea)]
+[Authorize(Roles = Constants.Roles.AdminRole)]
+public class UsersController : Controller
+{
+    private readonly ApplicationDbContext _db;
+    private readonly RoleManager<IdentityRole> _roleManager;
+    private readonly UserManager<IdentityUser> _userManager;
+
+    public UsersController(UserManager<IdentityUser> userManager, RoleManager<IdentityRole> roleManager,
+        ApplicationDbContext db)
+    {
+        _userManager = userManager;
+        _roleManager = roleManager;
+        _db = db;
+    }
+    
+    public async Task<IActionResult> Index()
+    {
+        var users = await _db.Users.ToListAsync();
+        return View(users);
+    }
+    
+    [HttpGet]
+    public async Task<IActionResult> Edit(string id)
+    {
+        if (id == null)
+        {
+            return NotFound();
+        }
+
+        var user = await _db.Users.FindAsync(id);
+        if (user == null)
+        {
+            return NotFound();
+        }
+
+        return View(user);
+    }
+
+    [HttpPost]
+    [ValidateAntiForgeryToken]
+    public async Task<IActionResult> Edit(string id, [Bind("Id,Email,FullName,Gender,DoB")] User user)
+    {
+        user.EmailConfirmed = true;
+        if (id != user.Id)
+        {
+            return NotFound();
+        }
+
+        if (ModelState.IsValid)
+        {
+            try
+            {
+                _db.Update(user);
+                await _db.SaveChangesAsync();
+            }
+            catch (DbUpdateConcurrencyException)
+            {
+                if (!UserExists(user.Id))
+                {
+                    return NotFound();
+                }
+                else
+                {
+                    throw;
+                }
+            }
+            return RedirectToAction(nameof(Index));
+        }
+        return View(user);
+    }
+
+    private bool UserExists(string id)
+    {
+        return _db.Users.Any(e => e.Id == id);
+    }
+
+    [HttpGet]
+    public async Task<IActionResult> Details(string id)
+    {
+        if (id == null)
+        {
+            return NotFound();
+        }
+
+        var user = await _db.Users.FindAsync(id);
+        if (user == null)
+        {
+            return NotFound();
+        }
+
+        return View(user);
+    }
+    
+    [HttpPost, ActionName("Delete")]
+    [ValidateAntiForgeryToken]
+    public async Task<IActionResult> DeleteConfirmed(string id)
+    {
+        var user = await _db.Users.FindAsync(id);
+        if (user != null) _db.Users.Remove(user);
+        await _db.SaveChangesAsync();
+        return RedirectToAction(nameof(Index));
+    }
+
+
+}
